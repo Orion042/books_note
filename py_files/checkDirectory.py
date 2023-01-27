5e1c8e2ca763f960b448ea7a2c34fd40928baeb6
Orion042 <orion042.code@gmail.com>
2023-01-28 04:56:02 +0900

    update file

--- py_files/checkDirectory.py
+++ py_files/checkDirectory.py
@@ -3,7 +3,7 @@ import os
 def createDirectory():
     path = os.getcwd()
     
-    directorys = ["md_files","notes","hello"]
+    directorys = ["md_files","notes"]
 
     for name in directorys:
         if not (os.path.isdir(path + "\\" + name)):
